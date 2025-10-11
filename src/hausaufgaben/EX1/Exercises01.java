23e20f8d435c0c078011add50f2e29a4b3c0f035
MinMari <159942867+min-mari@users.noreply.github.com>
2025-10-11 18:06:29 +0200

    Commit Addition

---
+++ src/hausaufgaben/EX1/Exercises01.java
@@ -0,0 +1,239 @@
+package hausaufgaben.EX1;
+import java.util.Scanner;
+
+public class Exercises01
+{
+
+    //todo Task 1
+    public void sayHelloWorld(){
+
+        System.out.println("Hello World!");
+    }
+
+    //todo Task 2
+    public void helloRobot(){
+        System.out.println("0123456789012345678901");
+        System.out.println("         __");
+        System.out.println(" _(\\    |@@|");
+        System.out.println("(__/\\__ \\--/ __");
+        System.out.println("   \\___|----|  |   __");
+        System.out.println("       \\ }{ /\\ )_ / _\\");
+        System.out.println("       /\\__/\\ \\__O (__");
+        System.out.println("      (--/\\--)    \\__/");
+        System.out.println("      _)(  )(_");
+        System.out.println("     `---''---`");
+    }
+
+    //todo Task 3
+    public void sumOfLiterals(){
+        char letter = 'Z';
+        int hexadezimal = 0xface;
+        int octal = 012;
+        long longNum = 80L;
+        float float1 = 44e-1f;
+        float float2 = 5.5f;
+        double double1 = 8.88e1;
+        double double2 = 99.9;
+
+        int sum = 0;
+
+        sum += letter;
+        sum += hexadezimal;
+        sum += octal;
+        sum += (int) longNum;
+        sum += (int) float1;
+        sum += (int) float2;
+        sum += (int) double1;
+        sum += (int) double2;
+
+        System.out.println(sum);
+    }
+
+    //todo Task 4
+    public void addTwoNumbers(){
+        Scanner input = new Scanner(System.in);
+        int n1 = input.nextInt();
+        int n2 = input.nextInt();
+        int sum = n1 + n2;
+        System.out.println(sum);
+    }
+
+    //todo Task 5
+    public void swapTwoNumbers(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Before Swap:");
+        System.out.print("x: ");
+        int x = sc.nextInt();
+        System.out.print("y: ");
+        int y = sc.nextInt();
+
+        x = x + y; //x=5+2 =7
+        y = x - y; //y=7-2 = 5
+        x = x - y; //x=7-5 = 2
+
+        System.out.println("After Swap:");
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+
+    //todo Task 6
+    public void compareTwoNumbers(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("n1: ");
+        int n1 = input.nextInt();
+        System.out.print("n2: ");
+        int n2 = input.nextInt();
+
+        if (n1 > n2)
+        {
+            System.out.println("n1 > n2");
+        }
+
+        else if (n2 > n1)
+        {
+            System.out.println("n2 > n1");
+        }
+
+        else
+        {
+            System.out.println("n1 == n2");
+        }
+    }
+
+    //todo Task 7
+    public void ratingSalesPerson(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter annual Revenue: ");
+
+        int revenue = input.nextInt();
+
+        if (revenue < 0 || revenue >= 100000)
+        {
+            System.out.println("Invalid Revenue");
+
+        }
+
+        else if (revenue < 20000)
+        {
+            System.out.println("Poor Sales Revenue");
+
+        }
+
+        else if (revenue < 50000)
+        {
+            System.out.println("Average Sales Revenue");
+
+        }
+
+        else if (revenue < 80000)
+        {
+            System.out.println("Good Sales Revenue");
+        }
+
+        else
+        {
+            System.out.println("Excellent Sales Revenue");
+        }
+    }
+
+    //todo Task 8
+    public void getCommissionRate(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter CommissionClass: ");
+        int commissionClass = input.nextInt();
+        double commissionRate;
+
+        switch (commissionClass)
+        {
+            case 1:
+                commissionRate = 0.01;
+                break;
+            case 2:
+                commissionRate = 0.02;
+                break;
+            case 3:
+                commissionRate = 0.03;
+                break;
+            case 4:
+                commissionRate = 0.04;
+                break;
+            default:
+                commissionRate = 0.0;
+                break;
+        }
+
+        System.out.println("Your Commission Rate was set to " + commissionRate);
+    }
+
+    //todo Task 9
+    public void leapyear(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Year: ");
+        int year = input.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            System.out.println("Leapyear");
+        }
+
+        else
+        {
+            System.out.println("Not a Leapyear");
+        }
+    }
+
+    //todo Task 10
+    //Task 10 muss man auf 3-Stellige Nummer angehen...ich habe das nicht berücksichtigt.
+    public void transposedNumbers(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Number: ");
+
+
+        int num = input.nextInt();
+        int hundreds = num / 100;
+        int tens = (num / 10) % 10;
+        int ones = num % 10;
+        int reversed = ones * 100 + tens * 10 + hundreds;
+        System.out.println(reversed);
+    }
+
+
+    public static void main(String[] args){
+        Exercises01 exercise1 = new Exercises01();
+
+        System.out.println("Task 1: Say Hello World");
+        exercise1.sayHelloWorld();
+
+        System.out.println("\nTask 2: Hello Robot");
+        exercise1.helloRobot();
+
+        System.out.println("\nTask 3: Literals");
+        exercise1.sumOfLiterals();
+
+        System.out.println("\nTask 4: Add two Numbers");
+        exercise1.addTwoNumbers();
+
+        System.out.println("\nTask 5: Swap two Numbers without using a helper variable");
+        exercise1.swapTwoNumbers();
+
+        System.out.println("\nTask 6: Compare two Numbers");
+        exercise1.compareTwoNumbers();
+
+        System.out.println("\nTask 7: Rating Sales Person");
+        exercise1.ratingSalesPerson();
+
+        System.out.println("\nTask 8: Commission Rate");
+        exercise1.getCommissionRate();
+
+        System.out.println("\nTask 9: Leapyear");
+        exercise1.leapyear();
+
+        System.out.println("\nTask 10: Transposed Numbers");
+        exercise1.transposedNumbers();
+    }
+}
---
+++ src/hausaufgaben/EX2/Exercises02.java
@@ -0,0 +1,284 @@
+package hausaufgaben.EX2;
+
+import java.util.Scanner;
+
+public class Exercises02 {
+
+    //todo Task 1
+    public void largestNumber()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number 1: ");
+        double inputNumber = scanner.nextDouble();
+
+
+        if (inputNumber <= 0)
+        {
+            System.out.println("No number entered.");
+            return;
+        }
+
+        double maxNum = inputNumber;
+        int count = 2;
+
+        while (true)
+        {
+            System.out.print("Number " + count + ": ");
+            inputNumber = scanner.nextDouble();
+
+            if (inputNumber <= 0)
+            {
+                break;
+            }
+
+            if (inputNumber > maxNum)
+            {
+                maxNum = inputNumber;
+            }
+
+            count++;
+        }
+
+        //Code für String output und .format aus google und schlussendlich aus gemini genommen, da ich mich sonst nicht ausgekannt habe
+        String output = String.format("%.2f", maxNum);
+        System.out.println("The largest number is " + output);
+    }
+
+    //todo Task 2
+    public void stairs()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n: ");
+
+        int n = scanner.nextInt();
+
+        if (n <= 0)
+        {
+            System.out.println("Invalid number!");
+            return;
+        }
+
+        int tempCount = 1;
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int column = 1; column <= row; column++)
+            {
+                System.out.print(tempCount++ + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //todo Task 3
+    public void printPyramid()
+    {
+
+        int rows = 6;
+        int starCounter = 0;
+
+        for (int currentRow = 1; currentRow <= rows; ++currentRow, starCounter = 0)
+        {
+            for (int spacing = 1; spacing <= rows - currentRow; ++spacing)
+            {
+                System.out.print(" ");
+            }
+
+            while (starCounter != 2 * currentRow - 1)
+            {
+                System.out.print("*");
+                ++starCounter;
+            }
+
+            System.out.println();
+        }
+    }
+
+    //todo Task 4
+    public void printRhombus()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("h: ");
+        int height = scanner.nextInt();
+
+        System.out.print("c: ");
+        char middleChar = scanner.next().charAt(0);
+
+        //Höhe muss 0 sein wenn /2 = ungerade
+        if (height % 2 == 0)
+        {
+            System.out.println("Invalid number!");
+            return;
+        }
+
+        int half = height / 2;  //bestimmen wieviel Zeilen unter oder ober der  Mitte...
+
+        //Top und Mitte
+        for (int row = 0; row <= half; row++)
+        {
+            //Spacing vor der row von Buchstaber
+            for (int spacing = 0; spacing < half - row; spacing++)
+            {
+                System.out.print(" ");
+            }
+
+            //Buchtaben symmetrisch
+            for (int offset = -row; offset <= row; offset++)
+            {
+                char currentChar = (char)(middleChar - Math.abs(offset));
+                System.out.print(currentChar);
+            }
+
+            System.out.println();
+        }
+
+        //bottom ohne mitte (da bei TOP) (symmetrisch)
+
+        for (int row = half - 1; row >= 0; row--) {
+            for (int spacing = 0; spacing < half - row; spacing++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int offset = -row; offset <= row; offset++)
+            {
+                //char currentChar = (char)(middleChar - Math.abs(offset)); mit hilfe von https://stackoverflow.com/questions/21653677/calculating-character-values-in-java-given-an-initial-character-and-an-offset
+                char currentChar = (char)(middleChar - Math.abs(offset));
+
+                System.out.print(currentChar);
+            }
+
+            System.out.println();
+        }
+    }
+
+    //todo Task 5
+    public void marks()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int gradeIndex = 1;
+        int numberOfValidGrades = 0;
+        int numberOfFails = 0;
+        int sumOfGrades = 0;
+
+        while (true)
+        {
+            System.out.print("Mark " + gradeIndex + ": ");
+            int enteredGrade = scanner.nextInt();
+
+
+            if (enteredGrade == 0)
+            {
+                //Durchschnitt (nicht = 0)
+                double average;
+
+                if (numberOfValidGrades == 0)
+                {
+                    average = 0.0;
+                }
+
+                else
+                {
+                    average = (double) sumOfGrades / numberOfValidGrades;
+                }
+
+                //string.format von task 1 genommen
+                String averageAsText = String.format("%.2f", average);
+
+                System.out.println("Average: " + averageAsText);
+                System.out.println("Negative marks: " + numberOfFails);
+                return;
+            }
+
+            // Ungültig + Error Message
+            if (enteredGrade < 1 || enteredGrade > 5)
+            {
+                System.out.println("Invalid mark!");
+                continue;
+            }
+
+            // Gültig - nehmen
+            sumOfGrades += enteredGrade;
+            numberOfValidGrades++;
+
+            if (enteredGrade == 5)
+            {
+                numberOfFails++;
+            }
+
+            gradeIndex++; //zu nächste Note gehen
+        }
+    }
+
+    //todo Task 6
+    public void happyNumbers()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n: ");
+
+        int originalNumber;
+        originalNumber = scanner.nextInt();
+        int currentValue = originalNumber;
+
+        //wenn der current Value nicht 1 oder 4 → WEITER
+        while (currentValue != 1 && currentValue != 4)
+        {
+            currentValue = sumOfSquaresOfDigits(currentValue);
+        }
+
+        //Ergebnise nachprüfen
+        if (currentValue == 1)
+        {
+            System.out.println("Happy number!");
+        }
+
+        if (currentValue == 4) {
+            System.out.println("Sad number!");
+        }
+    }
+
+    // quadrierte Ziffernsummen berechnen, hier habe ich mir Hilfe geholt von Geeksforgeeks: https://www.geeksforgeeks.org/dsa/happy-numbers/
+    private int sumOfSquaresOfDigits(int value) {
+        int sumOfSquares = 0;
+
+        while (value > 0)
+        {
+            //letztes Ziffer holen
+            int lastDigit = value % 10;
+
+            //quadrieren
+            int square = lastDigit * lastDigit;
+            sumOfSquares = sumOfSquares + square;
+
+            //letztes Ziffer wegschneiden
+            value = value / 10;
+        }
+
+        return sumOfSquares;
+    }
+
+    public static void main(String[] args){
+        Exercises02 exercise2 = new Exercises02();
+
+        System.out.println("Task 1: Largest Number");
+        exercise2.largestNumber();
+
+        System.out.println("\nTask 2: Stairs");
+        exercise2.stairs();
+
+        System.out.println("\nTask 3: Pyramide");
+        exercise2.printPyramid();
+
+        System.out.println("\nTask 4: Raute");
+        exercise2.printRhombus();
+
+        System.out.println("\nTask 5: Notendurchschnitt");
+        exercise2.marks();
+
+        System.out.println("\nTask 6: Fröhliche Zahlen");
+        exercise2.happyNumbers();
+    }
+}
