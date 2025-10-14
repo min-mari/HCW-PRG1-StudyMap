fd3ba0af0dca1c147887af731ebf60b4baa90859
MinMari <159942867+min-mari@users.noreply.github.com>
2025-10-14 19:58:06 +0200

    Latest update commit, 14.10.25; 19:57

--- src/example_tasks/RND5_FloatCompareExample.java
+++ src/example_tasks/RND5_FloatCompareExample.java
@@ -12,10 +12,10 @@ public class RND5_FloatCompareExample
         System.out.println("Direkter Vergleich (==): " + (a == b));
 
         //Rundung eps - verhindert rundungsfehler
-        final double eps = 1e-9;
+        final double eps = 1e-9; // Epsilon: kleiner Toleranzwert für float & double-Vergleiche
         boolean equalWithEps = Math.abs(a - b) < eps;
 
-        System.out.println("Vergleich mit eps: " + equalWithEps);
+        System.out.println("Vergleich: " + equalWithEps);
     }
 
 }
